6ea7f034cfb707440678e4e62c3c0f5afab5552d
Chazz Yu <chazz.q.yu@gmail.com>
2026-05-19 19:15:27 -0700

    chore: game header

--- include/game.h
+++ include/game.h
@@ -1,6 +1,27 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdbool.h>
+#include <stdint.h>
 
+#define BOARD_SIZE 8
+
+typedef enum {
+    EMPTY,
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING
+} PieceType;
+
+typedef struct {
+    int8_t board[BOARD_SIZE][BOARD_SIZE];
+    bool white_turn;
+} ChessGame;
+
+void init_game(ChessGame *game);
+bool make_move(ChessGame *game, int from_x, int from_y, int to_x, int to_y);
 
 #endif
